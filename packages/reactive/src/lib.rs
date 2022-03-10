ad32a7d59b1b3437c0a355d48eb3256478f4bcca
michaelbguo <michaelbguo@tencent.com>
2022-03-10 11:21:03 +0800

    feat(observer): rust observer

---
+++ packages/reactive/src/lib.rs
@@ -0,0 +1,32 @@
+mod observer;
+mod subject;
+
+#[cfg(test)]
+mod tests {
+    use crate::observer::{Listener, Observer};
+    use crate::subject::Subject;
+
+    #[test]
+    fn it_works() {
+        let mut subject = Subject::default();
+        let jack = Listener {
+            id: "jack".to_string(),
+        };
+        let tom = Listener {
+            id: "tom".to_string(),
+        };
+
+        let msg = "hello msg";
+        subject.subscribe(jack);
+        subject.subscribe(tom);
+
+        subject.publish(&msg);
+
+        subject.unsubscribe("tom");
+        subject.publish("some data");
+
+        subject.clear();
+
+        println!("Subject {:?}", subject);
+    }
+}
