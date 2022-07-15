ea59b9ca1d2e234ece0fda0b9a1dc37e33353472
michaelbguo <michaelbguo@tencent.com>
2022-07-15 14:14:27 +0800

    feat: update nestjs project

---
+++ apps/backend/src/config/env.config.ts
@@ -0,0 +1,10 @@
+export interface EnvConfig {
+  APP_NAME: string;
+  APP_PORT: number;
+  SQL_PORT: number;
+  SQL_HOST: string;
+  REDIS_PORT: number;
+  REDIS_HOST: string;
+  MONGO_PORT: number;
+  MONGO_HOST: string;
+}
