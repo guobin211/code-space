bf850e766985a147e86ae130be5daceb2f5cbf0f
michaelbguo <michaelbguo@tencent.com>
2022-09-22 15:53:11 +0800

    feat: add getServerSideProps

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,26 @@
+import { MouseEventHandler } from 'react';
 import { Button } from 'ui/src';
+import { GetServerSideProps } from 'next';
 
-export default function Web() {
+const Web = () => {
+  const handleClick: MouseEventHandler = (event) => {
+    console.log('clicked', event);
+  };
   return (
     <div>
       <h1>Web</h1>
-      <Button>Hello Web</Button>
+      <Button onClick={handleClick}>Hello Web</Button>
     </div>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { query } = ctx;
+  return {
+    props: {
+      query,
+    },
+  };
+};
+
+export default Web;
