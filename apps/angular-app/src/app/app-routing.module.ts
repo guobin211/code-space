3fbb0aaf0313df3691d448f8b860a0536ac4d9e6
michaelbguo <michaelbguo@tencent.com>
2022-09-25 12:30:49 +0800

    feat: add angular app

---
+++ apps/angular-app/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabledBlocking'
+})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ apps/angular-app/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { AppShellComponent } from './app-shell/app-shell.component';
+
+export const routes: Routes = [ { path: 'shell', component: AppShellComponent }];
