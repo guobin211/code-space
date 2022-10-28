9f67cca02e5ed13956acdb0faea51de2efe091f3
michaelbguo <michaelbguo@tencent.com>
2022-10-28 16:47:25 +0800

    feat: update pkg

---
+++ tests/mask.test.ts
@@ -0,0 +1,24 @@
+import { test, assert } from 'vitest';
+
+const renderEmail = (email: string) => {
+  if (email) {
+    let skip = false;
+    return email.split('').reduce((prev: string, next: string, index: number) => {
+      if (next === '@') {
+        skip = true;
+      }
+      if (skip) return `${prev}${next}`;
+      if (index > 3) {
+        return `${prev}*`;
+      }
+      return `${prev}${next}`;
+    }, '');
+  }
+  return '';
+};
+
+test('renderEmail', () => {
+  assert.equal(renderEmail('844922881@qq.com'), '8449*****@qq.com');
+  assert.equal(renderEmail('8449@qq.com'), '8449@qq.com');
+  assert.equal(renderEmail('84@qq.com'), '84@qq.com');
+});
