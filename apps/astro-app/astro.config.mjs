bfa8934fe0c4164f9c2222b40ea9d6706b346b49
michaelbguo <michaelbguo@tencent.com>
2023-03-11 15:44:52 +0800

    feat: remove vite-app

--- apps/astro-app/astro.config.mjs
+++ apps/astro-app/astro.config.mjs
@@ -3,7 +3,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-  ]
+  integrations: [react()],
+  server: {
+    port: 4201,
+  },
 });
--- apps/vite-app/src/entry.client.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { getClientProps, getRootElement } from './shared';
-
-const rootElement = getRootElement();
-const appProps = getClientProps();
-
-ReactDOM.hydrateRoot(rootElement, <App {...appProps} />);
